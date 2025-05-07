611decb57261cb7f60e25acdfd3c6a3c5b70697f
murali <murali@DESKTOP-37D2O36>
2025-05-07 13:08:54 +0530

    Updated...........!

---
+++ Dummy_Project/src/test/java/dummy_package/DummyTest.java
@@ -0,0 +1,13 @@
+package dummy_package;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DummyTest {
+
+	
+	@Test
+	public void addDummy() {
+		Reporter.log("Dummy Test is executed", true);
+	}
+}
